c9eb641f81e9f73b62e863175d1b6f57877a8906
SarahKapalka <sarahnj73bc@gmail.com>
2022-06-15 21:54:01 +0000

    edit

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 console.log(__dirname);
@@ -8,8 +10,16 @@ app.get('/', (req, res)=>{
 
 app.use( "/public",express.static(__dirname+"/public"));
 
+app.get('/json', (req, res) => {
+  process.env.MESSAGE_STYLE === 'uppercase' ? 
+    res.json({ "message": "HELLO JSON" }):
+    res.json({ "message": "Hello json" })
+})
 
-
+app.use((res,req,next)=>{
+ console.log(req.method+" "+req.path+" - "+req.ip);
+ next()
+})
 
 
 
