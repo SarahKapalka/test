8fbdb2cdcde78b22ca919ee39417975d4227d6e2
SarahKapalka <sarahnj73bc@gmail.com>
2022-06-15 19:29:23 +0000

    fourth commit

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
-console.log("Hello World")
+console.log(__dirname);
 
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+ "/views/index.html")
+});
 
-
+app.use( "/public",express.static(__dirname+"/public"));
 
 
 
