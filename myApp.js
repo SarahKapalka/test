13e178d0e054b78abd45ece9bdc7d0c9db563298
SarahKapalka <sarahnj73bc@gmail.com>
2022-06-17 16:21:56 +0000

    final commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
-
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 console.log(__dirname);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+ });
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+ "/views/index.html")
@@ -14,26 +22,26 @@ app.get('/json', (req, res) => {
   process.env.MESSAGE_STYLE === 'uppercase' ? 
     res.json({ "message": "HELLO JSON" }):
     res.json({ "message": "Hello json" })
-})
+});
 
-app.use((res,req,next)=>{
- console.log(req.method+" "+req.path+" - "+req.ip);
- next()
-})
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({"time": req.time});
+});
 
+app.get("/:text/echo", (req,res)=>{
+ res.json({"echo": req.params.text});
+ console.log(req.params);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.route("/name").get((req,res)=>{
+ res.json({"name": `${req.query.first} ${req.query.last}`});
+ console.log(req.query);
+}).post((req,res)=>{
+res.json({"name": `${req.body.first} ${req.body.last}`});
+});
 
 
 
